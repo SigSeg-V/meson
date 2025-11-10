SigSeg-V/meson

commit 6ee583e119b432fee03f908547729d5da030397e

allow to compare multiple version with version_compare

mesonbuild/interpreter/primitives/string.py
@@ -7,7 +7,7 @@ import os
 
 import typing as T
 
-from ...mesonlib import version_compare
+from ...mesonlib import version_compare, version_compare_many
 from ...interpreterbase import (
     ObjectHolder,
     MesonOperator,
@@ -169,9 +169,11 @@ class StringHolder(ObjectHolder[str]):
         return re.sub(r'[^a-zA-Z0-9]', '_', self.held_object)
 
     @noKwargs
-    @typed_pos_args('str.version_compare', str)
-    def version_compare_method(self, args: T.Tuple[str], kwargs: TYPE_kwargs) -> bool:
-        return version_compare(self.held_object, args[0])
+    @typed_pos_args('str.version_compare', varargs=str, min_varargs=1)
+    def version_compare_method(self, args: T.Tuple[T.List[str]], kwargs: TYPE_kwargs) -> bool:
+        if len(args[0]) > 1:
+            FeatureNew.single_use('version_compare() with multiple arguments', '1.8.0', self.subproject, location=self.current_node)
+        return version_compare_many(self.held_object, args[0])[0]
 
     @staticmethod
     def _op_div(this: str, other: str) -> str:

docs/yaml/elementary/str.yml
@@ -306,6 +306,12 @@ methods:
 
     It is best to be unambiguous and specify the full revision level to compare.
 
+    *Since 1.8.0* multiple versions can be compared:
+
+    ```meson
+    '3.6'.version_compare('>=3', '<4.0') == true
+    ```
+
   posargs:
     compare_string:
       type: str

test cases/common/35 string operations/meson.build
@@ -79,6 +79,11 @@ assert(not version_number.version_compare('!=1.2.8'), 'Version_compare neq broke
 assert(version_number.version_compare('<2.0'), 'Version_compare major less broken')
 assert(version_number.version_compare('>0.9'), 'Version_compare major greater broken')
 
+assert(version_number.version_compare('>1.2', '<1.3'))
+assert(not version_number.version_compare('>1.2', '>1.3'))
+assert(not version_number.version_compare('<1.2', '<1.3'))
+assert(version_number.version_compare('>1.0', '>1.2'))
+
 assert(' spaces	tabs	'.strip() == 'spaces	tabs', 'Spaces and tabs badly stripped')
 assert('''
 multiline string	'''.strip() == '''multiline string''', 'Newlines badly stripped')

test cases/d/11 dub/meson.build
@@ -6,7 +6,7 @@ if not dub_exe.found()
 endif
 
 dub_ver = dub_exe.version()
-if dub_ver.version_compare('>1.31.1') and dub_ver.version_compare('<1.35.0')
+if dub_ver.version_compare('>1.31.1', '<1.35.0')
   error('MESON_SKIP_TEST: Incompatible Dub version ' + dub_ver)
 endif
 

test cases/d/14 dub with deps/meson.build
@@ -6,7 +6,7 @@ if not dub_exe.found()
 endif
 
 dub_ver = dub_exe.version()
-if dub_ver.version_compare('>1.31.1') and dub_ver.version_compare('<1.35.0')
+if dub_ver.version_compare('>1.31.1', '<1.35.0')
   error('MESON_SKIP_TEST: Incompatible Dub version')
 endif
 

test cases/osx/3 has function xcode8/meson.build
@@ -12,7 +12,7 @@ sdk_args = ['-isysroot', '/Applications/Xcode.app/Contents/Developer/Platforms/M
 args_10_12 = ['-mmacosx-version-min=10.13'] + sdk_args
 
 # Test requires XCode 8 which has the MacOSX 10.12 SDK
-if cc.version().version_compare('>=8.0') and cc.version().version_compare('<8.1')
+if cc.version().version_compare('>=8.0', '<8.1')
   if cc.has_function('clock_gettime', args : args_10_11, prefix : '#include <time.h>')
     error('Should not have found clock_gettime via <time.h> when targeting Mac OS X 10.11')
   endif

commit f10ad9c5fc4b052a5cbedb3d62c508b4b305f38d

make str and meson.version() API align again.
Regression in commit 6ee583e119b432fee03f908547729d5da030397e.
str.version_compare() accepts varargs unless the str was defined via
meson.version(), which isn't supposed to be a visible end user
distinction.

That being said, it makes no sense to support the special casing here.

- FeatureNew only compares conditions with min, not with max

- Having multiple min conditions is illogical and there's no need to
  support it.

So we allow varargs for parity, but debug log that we can't handle it
specially and refrain from setting tmp_meson_version at all.

Fixes: https://github.com/mesonbuild/meson/issues/15217

mesonbuild/interpreter/primitives/string.py
@@ -8,7 +8,7 @@ import os
 import typing as T
 
 from ... import mlog
-from ...mesonlib import version_compare, version_compare_many, underscorify
+from ...mesonlib import version_compare_many, underscorify
 from ...interpreterbase import (
     InterpreterObject,
     MesonOperator,
@@ -198,18 +198,26 @@ class MesonVersionString(str):
 
 class MesonVersionStringHolder(StringHolder):
     @noKwargs
-    @typed_pos_args('str.version_compare', str)
     @InterpreterObject.method('version_compare')
-    def version_compare_method(self, args: T.Tuple[str], kwargs: TYPE_kwargs) -> bool:
+    @typed_pos_args('str.version_compare', varargs=str, min_varargs=1)
+    def version_compare_method(self, args: T.Tuple[T.List[str]], kwargs: TYPE_kwargs) -> bool:
         unsupported = []
-        if not args[0].strip().startswith('>'):
-            unsupported.append('non-upper-bounds (> or >=) constraints')
+        for constraint in args[0]:
+            if not constraint.strip().startswith('>'):
+                unsupported.append('non-upper-bounds (> or >=) constraints')
+        if len(args[0]) > 1:
+            FeatureNew.single_use('meson.version().version_compare() with multiple arguments', '1.10.0',
+                                  self.subproject, 'From 1.8.0 - 1.9.* it failed to match str.version_compare',
+                                  location=self.current_node)
+            unsupported.append('multiple arguments')
+        else:
+            self.interpreter.tmp_meson_version = args[0][0]
         if unsupported:
             mlog.debug('meson.version().version_compare() with', ' or '.join(unsupported),
                        'does not support overriding minimum meson_version checks.')
 
-        self.interpreter.tmp_meson_version = args[0]
-        return version_compare(self.held_object, args[0])
+        return version_compare_many(self.held_object, args[0])[0]
+
 
 # These special subclasses of string exist to cover the case where a dependency
 # exports a string variable interchangeable with a system dependency. This